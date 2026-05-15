69ef61d5942f6b5d3697e04c3cddd1ed4260e321
viking770 <singh.viking770@gmail.com>
2026-05-15 09:10:50 +0000

    All methods included

--- DSA/LinkedList/LinkedList.java
+++ DSA/LinkedList/LinkedList.java
@@ -29,7 +29,32 @@ public class LinkedList{
         return temp;
     }
 
-    public void append(int data){
+    public boolean set(int index, int data){
+        Node temp = get(index);
+        if(temp!=null){
+            temp.data = data;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean insert(int index, int data){
+        if(index <0 || index >= size)
+            return false;
+        if(index == 1){
+            return addAtHead(data);
+        }
+        if(index == size)
+            return append(data);
+        Node newNode = new Node(data);
+        Node temp = get(index-1);
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
+        return true;
+    }
+
+    public boolean append(int data){
         Node newNode = new Node(data);
         if(size == 0){
             head = newNode;
@@ -39,6 +64,7 @@ public class LinkedList{
             tail= newNode;
         }
         size++;
+        return true;
     }
 
     public void printList(){
@@ -49,7 +75,7 @@ public class LinkedList{
         }
     }
 
-    public void addAtHead(int data){
+    public boolean addAtHead(int data){
         Node newHead = new Node(data);
         if(size==0){
             head = newHead;
@@ -59,6 +85,7 @@ public class LinkedList{
             head = newHead;
         }
         size++;
+        return true;
     }
 
     public Node removeFirst(){
@@ -91,11 +118,41 @@ public class LinkedList{
         return temp;
     }
 
+    public Node remove(int index){
+        if(index<0 || index >= size)
+            return null;
+        if(index == size-1)
+            return removeLast();
+        Node prev = get(index-1);
+        Node temp = prev.next;
+        prev.next = temp.next;
+        temp.next = null;
+        size--;
+        return temp;
+    }
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+        Node after = temp.next;
+        Node before = null;
+        for (int i = 0; i < size; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+    }
+
     public static void main(String[] args){
         LinkedList list = new LinkedList(10);
         list.append(20);
         list.addAtHead(5);
         list.printList();
         System.out.println(list.get(1).data);
+        list.set(0, 2);
+        list.insert(2, 15);
+        list.printList();
     }
 }
