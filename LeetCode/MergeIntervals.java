347d8bc6120979499973fd81f392623aa6559e76
viking770 <singh.viking770@gmail.com>
2026-05-17 11:32:43 +0000

    code refractoring

--- LeetCode/MergeIntervals.java
+++ LeetCode/MergeIntervals.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class MergeIntervals {
     
-    public int[][] merge(int[][] intervals) {
+    public static int[][] merge(int[][] intervals) {
         if(intervals.length<=1)
             return intervals;
         Arrays.sort(intervals, (a,b)-> Integer.compare(a[0],b[0]));
@@ -22,7 +22,7 @@ public class MergeIntervals {
     }
 
     public static void main(String args[]){
-        int[][] intervals = [[1,3],[2,6],[8,10],[15,18]];
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
         System.out.println(merge(intervals));
     }
 }
